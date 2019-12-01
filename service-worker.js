96e1ae2bb22fe4f27ebc4da368e93b7520d220e6
codeghoul <jayesh.patel.2197@gmail.com>
2019-12-01 12:18:18 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sorting-visualizer/precache-manifest.238e2ec738c9b3a2cb92388e7072ad96.js"
+  "/sorting-visualizer/precache-manifest.2265b96ee7eef74fe54922d5fca4edaf.js"
 );
 
 self.addEventListener('message', (event) => {
